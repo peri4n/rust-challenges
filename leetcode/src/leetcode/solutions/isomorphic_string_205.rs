e136f8010cb0aa0273909843e0c572784255d924
Fabian Bull <spam+github@fbull.de>
2023-03-23 21:09:23 +0100

    Add LeetCode problem 205: Isomorphic strings

---
+++ leetcode/src/leetcode/solutions/isomorphic_string_205.rs
@@ -0,0 +1,63 @@
+use std::collections::HashMap;
+
+pub fn is_isomorphic(s: String, t: String) -> bool {
+    if s.len() != t.len() {
+        return false;
+    }
+
+    let t_chars: Vec<char> = t.chars().collect();
+
+    let mut seen = HashMap::new();
+    for (i, c) in s.char_indices() {
+        if let Some(&x) = seen.get(&c) {
+            if x != t_chars[i] {
+                return false;
+            }
+        } else {
+            if seen.values().any(|&x| x == t_chars[i]) {
+                return false;
+            } else {
+                seen.insert(c, t_chars[i]);
+            }
+        }
+    }
+
+    return true;
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        assert_eq!(
+            is_isomorphic(String::from("egg"), String::from("add")),
+            true
+        );
+    }
+
+    #[test]
+    fn cases2() {
+        assert_eq!(
+            is_isomorphic(String::from("foo"), String::from("bar")),
+            false
+        );
+    }
+
+    #[test]
+    fn cases3() {
+        assert_eq!(
+            is_isomorphic(String::from("paper"), String::from("title")),
+            true
+        );
+    }
+
+    #[test]
+    fn cases4() {
+        assert_eq!(
+            is_isomorphic(String::from("badc"), String::from("baba")),
+            false
+        );
+    }
+}
--- leetcode/src/leetcode/solutions/mod.rs
+++ leetcode/src/leetcode/solutions/mod.rs
@@ -1,4 +1,5 @@
 pub mod can_place_flowers_605;
+pub mod isomorphic_string_205;
 pub mod nr_of_zero_filled_subarrays_2348;
 pub mod single_number_136;
 pub mod two_sum_1;
