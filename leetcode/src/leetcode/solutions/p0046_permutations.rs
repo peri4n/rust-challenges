175d4b450d12f4871b79faa45f07e52fce7ed090
Fabian Bull <spam+github@fbull.de>
2023-04-24 20:57:15 +0200

    Add LeetCode Problem 46: Permutations

---
+++ leetcode/src/leetcode/solutions/p0046_permutations.rs
@@ -0,0 +1,54 @@
+pub fn permute(values: Vec<i32>) -> Vec<Vec<i32>> {
+    fn helper(values: &mut Vec<i32>) -> Vec<Vec<i32>> {
+        if values.len() <= 1 {
+            return vec![values.clone()];
+        }
+
+        let mut res = vec![];
+        for _ in 0..values.len() {
+            let first = values.remove(0); // pop
+            let mut perms = helper(values);
+            
+            for p in &mut perms {
+                p.push(first);
+            }
+
+            res.extend(perms);
+            values.push(first); // append what has been poped
+        }
+        res
+    }
+
+    let mut temp = values.clone();
+    helper(&mut temp)
+}
+
+#[cfg(test)]
+mod test {
+    use super::permute;
+
+    #[test]
+    fn case1() {
+        assert_eq!(
+            permute(vec![1, 2, 3]),
+            vec![
+                vec![3, 2, 1],
+                vec![2, 3, 1],
+                vec![1, 3, 2],
+                vec![3, 1, 2],
+                vec![2, 1, 3],
+                vec![1, 2, 3]
+            ]
+        );
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(permute(vec![1, 0]), vec![vec![0, 1], vec![1, 0]]);
+    }
+
+    #[test]
+    fn case3() {
+        assert_eq!(permute(vec![]), vec![vec![]]);
+    }
+}
