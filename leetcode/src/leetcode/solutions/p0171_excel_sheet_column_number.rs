e3760292765770331f4ef6ab875d01077da0e9eb
Fabian Bull <spam+github@fbull.de>
2023-04-26 19:59:30 +0200

    Add LeetCode Problem 171: Excel Sheet Column Number

---
+++ leetcode/src/leetcode/solutions/p0171_excel_sheet_column_number.rs
@@ -0,0 +1,20 @@
+pub fn title_to_number(column_title: String) -> i32 {
+    column_title
+    .chars()
+    .map(|c| c as i32 - 'A' as i32 + 1)
+    .fold(0, |prev, next| prev * 26 + next)
+}
+
+#[cfg(test)]
+mod test {
+    use super::title_to_number;
+
+
+    #[test]
+    fn cases() {
+        assert_eq!(title_to_number("A".into()), 1);
+        assert_eq!(title_to_number("B".into()), 2);
+        assert_eq!(title_to_number("AB".into()), 28);
+        assert_eq!(title_to_number("ZY".into()), 701);
+    }
+}
