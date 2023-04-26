f0366277615d36867dc2905c3040194b049d6135
Fabian Bull <spam+github@fbull.de>
2023-04-26 08:48:29 +0200

    Add LeetCode Problem 256: Add Digits

--- leetcode/src/leetcode/solutions/mod.rs
+++ leetcode/src/leetcode/solutions/mod.rs
@@ -10,6 +10,7 @@ pub mod p0136_single_number;
 pub mod p0202_happy_number;
 pub mod p0205_isomorphic_string;
 pub mod p0219_contains_duplicate_2;
+pub mod p0256_add_digits;
 pub mod p0290_word_pattern;
 pub mod p0605_can_place_flowers;
 pub mod p1046_last_stone_weight;
---
+++ leetcode/src/leetcode/solutions/p0256_add_digits.rs
@@ -0,0 +1,41 @@
+pub fn add_digits(num: i32) -> i32 {
+    let mut sum = digit_sum(num);
+    println!("{}", sum);
+
+    while sum >= 10 {
+        sum = digit_sum(sum);
+    }
+    sum
+}
+
+fn digit_sum(num: i32) -> i32 {
+    let mut num = num;
+    let mut sum = 0;
+
+    while num > 0 {
+        sum += num % 10;
+        num /= 10;
+    }
+
+    sum
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn cases() {
+        assert_eq!(add_digits(38), 2);
+        assert_eq!(add_digits(0), 0);
+        assert_eq!(add_digits(10), 1);
+    }
+
+    #[test]
+    fn digit_sum_test() {
+        assert_eq!(digit_sum(38), 11);
+        assert_eq!(digit_sum(11), 2);
+
+        assert_eq!(digit_sum(10), 1);
+    }
+}
