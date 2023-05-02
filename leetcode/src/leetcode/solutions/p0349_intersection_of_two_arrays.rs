1ff5d0605f0cef81f21dbbcdad474ee457ae46b0
Fabian Bull <spam+github@fbull.de>
2023-05-02 08:56:37 +0200

    Add LeetCode problem 349: Intersection of Two Arrays

---
+++ leetcode/src/leetcode/solutions/p0349_intersection_of_two_arrays.rs
@@ -0,0 +1,22 @@
+    use std::collections::HashSet;
+    use std::iter::FromIterator;
+
+    pub fn intersection(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
+        let set1: HashSet<i32> = HashSet::from_iter(nums1);
+        let set2: HashSet<i32> = HashSet::from_iter(nums2);
+
+        set1.intersection(&set2).copied().collect()
+    }
+
+#[cfg(test)]
+mod test {
+    use super::intersection;
+
+    #[test]
+    fn cases() {
+        assert_eq!(intersection(vec![1, 2, 2, 1], vec![2]), vec![2]);
+        let mut inter = intersection(vec![4, 9, 5], vec![9, 4, 9, 8, 4]);
+        inter.sort();
+        assert_eq!(inter, vec![4, 9]);
+    }
+}
