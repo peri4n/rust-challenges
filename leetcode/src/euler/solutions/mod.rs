5d4e81bc4e02eb365ebce1055342551d5fdf13e3
Fabian Bull <spam+github@fbull.de>
2023-05-02 21:14:04 +0200

    Add Project Euler Problem 2

--- leetcode/src/euler/solutions/mod.rs
+++ leetcode/src/euler/solutions/mod.rs
@@ -1,1 +1,2 @@
 pub mod p0001_multiples_of_3_and_5;
+pub mod p0002_even_fibonacci_numbers;
---
+++ leetcode/src/euler/solutions/p0002_even_fibonacci_numbers.rs
@@ -0,0 +1,42 @@
+pub struct Fib {
+    previous: i32,
+    current: i32,
+}
+
+impl Fib {
+    pub fn new() -> Self {
+        Self {
+            previous: 0,
+            current: 1,
+        }
+    }
+}
+
+impl Iterator for Fib {
+    type Item = i32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let sum = self.current + self.previous;
+        self.previous = self.current;
+        self.current = sum;
+
+        Some(self.previous)
+    }
+}
+
+pub fn even_fibonacci() -> i32 {
+    Fib::new()
+        .take_while(|&f| f < 4_000_000)
+        .filter(|&f| f % 2 == 0)
+        .sum()
+}
+
+#[cfg(test)]
+mod test {
+    use super::even_fibonacci;
+
+    #[test]
+    fn solution() {
+        assert_eq!(even_fibonacci(), 4613732);
+    }
+}
