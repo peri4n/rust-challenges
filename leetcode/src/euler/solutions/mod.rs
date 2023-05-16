9d2a860506090a3f7c8daada028cb7fceaf71a8c
Fabian Bull <spam+github@fbull.de>
2023-05-16 10:07:16 +0200

    Add Project Euler problem 39: Integer Right Triangles

--- leetcode/src/euler/solutions/mod.rs
+++ leetcode/src/euler/solutions/mod.rs
@@ -2,3 +2,4 @@ pub mod p0001_multiples_of_3_and_5;
 pub mod p0002_even_fibonacci_numbers;
 pub mod p0003_largest_prime_factor;
 pub mod p0026_reciprocal_cylcles;
+pub mod p0039_integer_right_triangles;
---
+++ leetcode/src/euler/solutions/p0039_integer_right_triangles.rs
@@ -0,0 +1,29 @@
+pub fn is_right_triangle(a: usize, b: usize, c: usize) -> bool {
+    a * a + b * b == c * c
+}
+
+pub fn biggest_perimeter(upper_limit: usize) -> usize {
+    let half_limit = upper_limit / 2;
+    let mut perimeters = vec![0; upper_limit];
+
+    for a in 1..half_limit {
+        for b in a..half_limit - 1 {
+            for c in b..(upper_limit - a - b) {
+                if is_right_triangle(a, b, c) {
+                    perimeters[a+b+c] += 1;
+                }
+            }
+        }
+    }
+    perimeters.into_iter().enumerate().max_by_key(|f| f.1).unwrap().0
+}
+
+#[cfg(test)]
+mod test {
+    use super::biggest_perimeter;
+
+    #[test]
+    fn solution() {
+        assert_eq!(biggest_perimeter(1000), 840);
+    }
+}
