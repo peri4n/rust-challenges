892b99b5e78d5df3d538e31113a3d2b21e7bbe9d
Fabian Bull <spam+github@fbull.de>
2023-06-05 14:19:09 +0200

    Add Advent Of Code problem day 2 of 2015

---
+++ src/aoc/y2015/day2.rs
@@ -0,0 +1,110 @@
+use nom::character::complete::char;
+use nom::character::complete::i32;
+use nom::IResult;
+use std::fs::File;
+use std::io::{self, BufRead};
+
+const INPUT_FILE: &str = "src/aoc/y2015/day2.txt";
+
+#[derive(Debug)]
+struct Packet {
+    width: i32,
+    height: i32,
+    length: i32,
+}
+
+impl Packet {
+    pub fn new(width: i32, height: i32, length: i32) -> Self {
+        Packet {
+            width,
+            height,
+            length,
+        }
+    }
+    pub fn paper(&self) -> i32 {
+        let all = 2 * self.width * self.height
+            + 2 * self.width * self.length
+            + 2 * self.length * self.height;
+
+        all + (self.width * self.length)
+            .min(self.width * self.height)
+            .min(self.length * self.height)
+    }
+
+    pub fn ribbon(&self) -> i32 {
+        let all = self.height * self.width * self.length;
+
+        all + 2 * (self.width + self.length)
+            .min(self.width + self.height)
+            .min(self.length + self.height)
+
+    }
+}
+
+fn parse_line(input: &str) -> IResult<&str, Packet> {
+    let (rest, length) = i32(input)?;
+    let (rest, _) = char('x')(rest)?;
+    let (rest, width) = i32(rest)?;
+    let (rest, _) = char('x')(rest)?;
+    let (rest, height) = i32(rest)?;
+    Ok((
+        rest,
+        Packet {
+            height,
+            width,
+            length,
+        },
+    ))
+}
+
+fn input() -> impl Iterator<Item=Packet> {
+    let file = File::open(INPUT_FILE).expect("Unable to read input file");
+    io::BufReader::new(file).lines().map(|line| {
+        let line = line.unwrap();
+
+        let (_, packet) = parse_line(&line).unwrap();
+        return packet;
+    })
+}
+
+fn day2_fst() -> i32 {
+    let mut sum = 0;
+    for packet in input() {
+        sum += packet.paper();
+    }
+
+    sum
+}
+
+fn day2_snd() -> i32 {
+    let mut sum = 0;
+    for packet in input() {
+        sum += packet.ribbon();
+    }
+
+    sum
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn paper_size() {
+        assert_eq!(Packet::new(2, 3, 4).paper(), 58);
+        assert_eq!(Packet::new(4, 3, 2).paper(), 58);
+        assert_eq!(Packet::new(2, 4, 3).paper(), 58);
+        assert_eq!(Packet::new(1, 1, 10).paper(), 43);
+        assert_eq!(Packet::new(10, 1, 1).paper(), 43);
+        assert_eq!(Packet::new(1, 10, 1).paper(), 43);
+    }
+
+    #[test]
+    fn solution_fst() {
+        assert_eq!(day2_fst(), 1586300);
+    }
+}
+    #[test]
+    fn solution_snd() {
+        assert_eq!(day2_snd(), 1586300);
+    }
