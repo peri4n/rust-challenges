648f550001318f27073c8443d5981076d5193475
Fabian Bull <spam+github@fbull.de>
2023-06-06 09:27:54 +0200

    Add LeetCode problem 344: Reverse String

---
+++ src/leetcode/solutions/p0344_reverse_string.rs
@@ -0,0 +1,32 @@
+pub fn reverse_string(s: &mut Vec<char>) {
+    let mut left = 0;
+    let mut right = s.len() - 1;
+
+    while left < right {
+        let tmp = s[left];
+        s[left] = s[right];
+        s[right] = tmp;
+        left += 1;
+        right -= 1;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn case1() {
+        let mut chars: Vec<char> = "hello".chars().collect();
+        reverse_string(&mut chars);
+        assert_eq!(chars, "olleh".chars().collect::<Vec<char>>());
+    }
+
+    #[test]
+    fn case2() {
+        let mut chars: Vec<char> = "Hannah".chars().collect();
+        reverse_string(&mut chars);
+        assert_eq!(chars, "hannaH".chars().collect::<Vec<char>>());
+    }
+}
