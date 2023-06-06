0fc6a586a24a85e798416a701df8eaf522bbaad9
Fabian Bull <spam+github@fbull.de>
2023-06-06 08:36:38 +0200

    Add LeetCode problem 20: Valid Parentheses

---
+++ src/leetcode/solutions/p0020_valid_parentheses.rs
@@ -0,0 +1,30 @@
+pub fn is_valid(s: String) -> bool {
+    let mut stack: Vec<char> = vec![];
+
+    for c in s.chars() {
+        match c {
+            '(' => stack.push(')'),
+            '[' => stack.push(']'),
+            '{' => stack.push('}'),
+            ')' | ']' | '}' if Some(c) != stack.pop() => return false,
+            _ => (),
+        }
+    }
+
+    return stack.is_empty();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn cases() {
+        assert_eq!(is_valid("()".to_string()), true);
+        assert_eq!(is_valid("()[]{}".to_string()), true);
+        assert_eq!(is_valid("{[]}".to_string()), true);
+
+        assert_eq!(is_valid("([".to_string()), false);
+        assert_eq!(is_valid("([)]".to_string()), false);
+    }
+}
