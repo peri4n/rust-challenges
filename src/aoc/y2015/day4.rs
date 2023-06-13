42ba3418ca13571f86c5e666f509eef5c9e78fe1
Fabian Bull <spam+github@fbull.de>
2023-06-13 11:29:07 +0200

    Add Advent Of Code 2015 day 4

---
+++ src/aoc/y2015/day4.rs
@@ -0,0 +1,42 @@
+use md5::compute;
+
+const INPUT: &str = "iwrupvqb";
+
+fn day4_fst() -> usize {
+    let mut i = 0;
+    loop {
+        let hash = compute([INPUT, &i.to_string()].concat());
+        if hash[0] == 0 && hash[1] == 0 && (hash[2] >> 4) == 0 {
+            return i;
+        }
+
+        i += 1;
+    }
+}
+
+fn day4_snd() -> usize {
+    let mut i = 0;
+    loop {
+        let hash = compute([INPUT, &i.to_string()].concat());
+        if hash[0] == 0 && hash[1] == 0 && hash[2] == 0 {
+            return i;
+        }
+
+        i += 1;
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn solution_fst() {
+        assert_eq!(day4_fst(), 346386);
+    }
+
+    #[test]
+    fn solution_snd() {
+        assert_eq!(day4_snd(), 9958218);
+    }
+}
