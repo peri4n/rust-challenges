1c612a61ff655360ec0ec53d44aacf4e67cfbab4
Fabian Bull <spam+github@fbull.de>
2023-10-11 14:22:14 +0200

    Add LeetCode Problem 1928: Build Array From Permutation

--- src/leetcode/solutions/mod.rs
+++ src/leetcode/solutions/mod.rs
@@ -40,6 +40,7 @@ pub mod p1480_running_sum_1d_array;
 pub mod p1768_merge_strings_alternately;
 pub mod p1791_find_center_of_star_graph;
 pub mod p1822_sign_of_the_product_of_an_array;
+pub mod p1920_build_array_from_permutation;
 pub mod p1929_concatenation_of_array;
 pub mod p2215_find_the_difference_of_two_arrays;
 pub mod p2224_minimum_number_of_operations_to_convert_time;
---
+++ src/leetcode/solutions/p1920_build_array_from_permutation.rs
@@ -0,0 +1,20 @@
+pub fn build_array(nums: Vec<i32>) -> Vec<i32> {
+    let mut res = vec![0; nums.len()];
+
+    for (i, x) in nums.iter().enumerate() {
+        res[i] = nums[nums[i] as usize];
+    }
+
+    res
+}
+
+#[cfg(test)]
+mod test {
+    use super::build_array;
+
+    #[test]
+    fn cases() {
+        assert_eq!(build_array(vec![0,2,1,5,3,4]), vec![0,1,2,4,5,3]);
+        assert_eq!(build_array(vec![5,0,1,2,3,4]), vec![4,5,0,1,2,3]);
+    }
+}
