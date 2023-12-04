cad45d1abc944279cb658ac96991b94323817b5f
Fabian Bull <spam+github@fbull.de>
2023-12-04 14:04:38 +0100

    Add Day 3 of AoC 2023 in Rust

---
+++ src/aoc/y2023/day3.rs
@@ -0,0 +1,125 @@
+use core::{iter::Iterator, ops::Range, str::Lines};
+use regex::{CaptureMatches, Match, Regex};
+
+lazy_static! {
+    static ref NUM_RE: Regex = Regex::new(r"\d+").unwrap();
+    static ref SYM_RE: Regex = Regex::new(r"[^.\d]").unwrap();
+}
+
+// Iterator for (above, cur, below) line windows
+
+type LineWindow<'a> = (Option<&'a str>, &'a str, Option<&'a str>);
+
+struct Windows<'a> {
+    lines: Lines<'a>,
+    buf: [Option<&'a str>; 2],
+}
+
+impl<'a> Iterator for Windows<'a> {
+    type Item = LineWindow<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let window = (self.buf[0].take(), self.buf[1].take()?, self.lines.next());
+        self.buf = [Some(window.1), window.2];
+        Some(window)
+    }
+}
+
+fn windows(input: &str) -> Windows {
+    let mut lines = input.lines();
+    let buf = [None, lines.next()];
+    Windows { lines, buf }
+}
+
+// Iterator for matches of a pattern around a given string
+
+struct Adjacent<'a, 'b> {
+    cur: CaptureMatches<'a, 'b>,
+    above: Option<CaptureMatches<'a, 'b>>,
+    below: Option<CaptureMatches<'a, 'b>>,
+    range: Range<usize>,
+}
+
+fn find_adjacent<'a, 'b>(
+    pat: &'a Regex,
+    w: LineWindow<'b>,
+    range: Range<usize>,
+) -> Adjacent<'a, 'b> {
+    let cur = pat.captures_iter(w.1);
+    let above = w.0.map(|above| pat.captures_iter(above));
+    let below = w.2.map(|below| pat.captures_iter(below));
+    Adjacent {
+        cur,
+        above,
+        below,
+        range,
+    }
+}
+
+impl<'a, 'b> Iterator for Adjacent<'a, 'b> {
+    type Item = Match<'b>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        for cap in &mut self.cur {
+            let cap = cap.get(0).unwrap();
+            if cap.start() == self.range.end || cap.end() == self.range.start {
+                return Some(cap);
+            }
+        }
+        for caps in [&mut self.above, &mut self.below].into_iter().flatten() {
+            for cap in caps {
+                let cap = cap.get(0).unwrap();
+                if cap.start() <= self.range.end && cap.end() >= self.range.start {
+                    return Some(cap);
+                }
+            }
+        }
+        None
+    }
+}
+
+fn sliding_windows_sum(input: &str, f: impl Fn(LineWindow) -> i64) -> i64 {
+    windows(input).map(f).sum()
+}
+
+fn parse_num(m: Match) -> i64 {
+    m.as_str().parse().unwrap()
+}
+
+pub fn day3_fst(input: &str) -> i64 {
+    sliding_windows_sum(input, |w @ (_, cur, _)| {
+        let nums = NUM_RE.captures_iter(cur).map(|m| m.get(0).unwrap());
+        let has_adj_sym = |m: &Match| find_adjacent(&SYM_RE, w, m.range()).count() > 0;
+        let part_nums = nums.filter(has_adj_sym).map(parse_num);
+        part_nums.sum()
+    })
+}
+
+pub fn day3_snd(input: &str) -> i64 {
+    sliding_windows_sum(input, |w @ (_, cur, _)| {
+        let gears = cur.chars().enumerate().filter(|p| p.1 == '*').map(|p| p.0);
+        let gear_ratio = |i| {
+            let mut adj_nums = find_adjacent(&NUM_RE, w, i..i + 1).map(parse_num);
+            let ratio = adj_nums.next()? * adj_nums.next()?;
+            Some(ratio).xor(adj_nums.next())
+        };
+        gears.flat_map(gear_ratio).sum()
+    })
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn solution_fst() {
+        let input = include_str!("../y2023/day3.txt");
+        assert_eq!(day3_fst(input), 521601);
+    }
+
+    #[test]
+    fn solution_snd() {
+        let input = include_str!("../y2023/day3.txt");
+        assert_eq!(day3_snd(input), 80694070);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
 pub mod aoc;
 pub mod euler;
 pub mod leetcode;
+
+#[macro_use]
+extern crate lazy_static;
