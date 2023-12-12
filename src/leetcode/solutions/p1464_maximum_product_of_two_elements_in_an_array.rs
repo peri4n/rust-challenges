07ca572485afcfdedc759e9244b3f3d6977a7932
Fabian Bull <spam+github@fbull.de>
2023-12-12 08:17:36 +0100

    Add LeedCode problem 1464 in Rust

---
+++ src/leetcode/solutions/p1464_maximum_product_of_two_elements_in_an_array.rs
@@ -0,0 +1,21 @@
+/// TODO This can be improved by only storing the two largest values in the heap
+use std::collections::BinaryHeap;
+
+pub fn max_product(nums: Vec<i32>) -> i32 {
+    let mut heap = BinaryHeap::from(nums);
+
+    (heap.pop().unwrap() - 1) * ( heap.pop().unwrap() - 1)
+
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_cases() {
+        assert_eq!(max_product(vec![3, 4, 5, 2]), 12);
+        assert_eq!(max_product(vec![1, 5, 4, 5]), 16);
+        assert_eq!(max_product(vec![3, 7]), 12);
+    }
+}
