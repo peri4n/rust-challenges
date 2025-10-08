201e7e4961a0fbce1d97ebabe1d1fb960c5ac2e1
Fabian Bull <spam+github@fbull.de>
2025-10-08 20:17:52 +0200

    Add day1 of Advent of Code 2017 in Rust

--- src/aoc/mod.rs
+++ src/aoc/mod.rs
@@ -1,5 +1,6 @@
 pub mod y2015;
 pub mod y2016;
+pub mod y2017;
 pub mod y2019;
 pub mod y2023;
 pub mod y2024;
---
+++ src/aoc/y2017/day1.rs
@@ -0,0 +1,52 @@
+use std::fs;
+
+
+const INPUT_FILE: &str = "src/aoc/y2017/day1.txt";
+
+fn input() -> Vec<u8> {
+    let content = fs::read_to_string(INPUT_FILE).expect("Should have been able to read the file");
+    content.chars()
+        .filter(|c| c.is_digit(10))
+        .map(|c| c.to_digit(10).unwrap() as u8).collect()
+}
+
+pub fn day1_fst() -> u32 {
+    let digits = input();
+    let mut sum = 0;
+
+    for i in 0..digits.len() {
+        if digits[i] == digits[(i + 1) % digits.len()] {
+            sum += digits[i] as u32;
+        }
+    }
+    sum
+}
+
+pub fn day1_snd() -> u32 {
+    let digits = input();
+    let mut sum = 0;
+    let step = digits.len() / 2;
+
+    for i in 0..digits.len() {
+        if digits[i] == digits[(i + step) % digits.len()] {
+            sum += digits[i] as u32;
+        }
+    }
+    sum
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn day1_solution_fst() {
+        assert_eq!(day1_fst(), 1102);
+    }
+
+    #[test]
+    fn day1_solution_snd() {
+        assert_eq!(day1_snd(), 1076);
+    }
+}
