2636bcc52f86459af5a97b62312e5a27fd9b77b9
Fabian Bull <spam+github@fbull.de>
2025-11-19 14:41:35 +0100

    Add day 25 of Advent of Code 2015 in Rust

---
+++ src/aoc/y2015/day25.rs
@@ -0,0 +1,35 @@
+pub fn day25_fst() -> u64 {
+    let mut code = 20151125u64;
+    let target_row = 3010;
+    let target_col = 3019;
+
+    let mut row = 1;
+    let mut column = 1;
+
+    while row != target_row || column != target_col {
+
+        code = (code * 252533) % 33554393;
+
+        if row == 1 {
+            row = column + 1;
+            column = 1;
+        } else {
+            row -= 1;
+            column += 1;
+        }
+
+    }
+
+    code
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn solution_fst() {
+        assert_eq!(day25_fst(), 8997277);
+    }
+}
