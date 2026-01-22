6e702f14f77ab95cc1b9aa3f3267f582932ee9e6
Fabian Bull <spam+github@fbull.de>
2026-01-22 08:20:38 +0100

    Improve LeetCode problem 1071

--- src/leetcode/solutions/p1071_greatest_common_divisor_of_strings.rs
+++ src/leetcode/solutions/p1071_greatest_common_divisor_of_strings.rs
@@ -1,37 +1,15 @@
+use num::Integer;
+
 pub fn gcd_of_strings(str1: String, str2: String) -> String {
-    if is_multiple(&str1, &str2) {
-        let g = gcd(str1.len(), str2.len());
-        return str1[0..g].to_owned();
+    if str1.clone() + &str2 != str2.clone() + &str1 {
+        return String::new(); // Early termination: Condition for no common divisor
     }
 
-    "".to_string()
-}
+    let len1 = str1.len();
+    let len2 = str2.len();
+    let g = len1.gcd(&len2);
 
-fn is_multiple(str1: &str, str2: &str) -> bool {
-    let chars1 = str1.chars();
-    let chars2 = str2.chars();
-
-    chars1
-        .clone()
-        .chain(chars2.clone())
-        .eq(chars2.chain(chars1))
-}
-
-fn gcd(a: usize, b: usize) -> usize {
-    let mut a = a;
-    let mut b = b;
-    loop {
-        if a == 0 {
-            return b;
-        }
-        if b == 0 {
-            return a;
-        }
-
-        let remainder = a % b;
-        a = b;
-        b = remainder;
-    }
+    str1[..g].to_string()
 }
 
 #[cfg(test)]
