410bdd4fae56653dac7d52f9064084b626a48bd7
Fabian Bull <spam+github@fbull.de>
2026-04-01 11:29:39 +0200

    Improve Aoc 2015 day 6

--- src/aoc/y2015/day6.rs
+++ src/aoc/y2015/day6.rs
@@ -1,13 +1,13 @@
 use nom::branch::alt;
 use nom::bytes::complete::tag;
-use nom::character::complete::{char, u32};
-use nom::combinator::value;
+use nom::character::complete::{char, u32 as nom_u32};
+use nom::combinator::{map, value};
 use nom::sequence::{separated_pair, terminated};
 use std::fs;
 
 use nom::{IResult, Parser};
 
-type Lights = Vec<Vec<u8>>;
+type Lights = Vec<Vec<u32>>;
 
 const INPUT_FILE: &str = "src/aoc/y2015/day6.txt";
 
@@ -20,66 +20,34 @@ enum Kind {
 
 #[derive(Debug)]
 struct Instruction {
-    top_left: (u32, u32),
-    bottom_right: (u32, u32),
+    top_left: (usize, usize),
+    bottom_right: (usize, usize),
     kind: Kind,
 }
 
 impl Instruction {
-    pub fn exec(&self, lights: &mut Lights) {
-        match self.kind {
-            Kind::Off => {
-                for i in self.top_left.0..=self.bottom_right.0 {
-                    for j in self.top_left.1..=self.bottom_right.1 {
-                        lights[i as usize][j as usize] = 0;
-                    }
-                }
-            }
-            Kind::On => {
-                for i in self.top_left.0..=self.bottom_right.0 {
-                    for j in self.top_left.1..=self.bottom_right.1 {
-                        lights[i as usize][j as usize] = 1;
-                    }
-                }
-            }
-            Kind::Toggle => {
-                for i in self.top_left.0..=self.bottom_right.0 {
-                    for j in self.top_left.1..=self.bottom_right.1 {
-                        lights[i as usize][j as usize] = 1 - lights[i as usize][j as usize]
-                    }
-                }
+    fn for_each_cell(&self, lights: &mut Lights, mut f: impl FnMut(&mut u32)) {
+        for row in &mut lights[self.top_left.0..=self.bottom_right.0] {
+            for cell in &mut row[self.top_left.1..=self.bottom_right.1] {
+                f(cell);
             }
         }
     }
 
-    pub fn exec2(&self, lights: &mut Lights) {
-        match self.kind {
-            Kind::Off => {
-                for i in self.top_left.0..=self.bottom_right.0 {
-                    for j in self.top_left.1..=self.bottom_right.1 {
-                        let brightness = &mut lights[i as usize][j as usize];
+    pub fn exec(&self, lights: &mut Lights) {
+        self.for_each_cell(lights, |cell| match self.kind {
+            Kind::Off => *cell = 0,
+            Kind::On => *cell = 1,
+            Kind::Toggle => *cell = 1 - *cell,
+        });
+    }
 
-                        if *brightness > 0 {
-                            *brightness -= 1;
-                        }
-                    }
-                }
-            }
-            Kind::On => {
-                for i in self.top_left.0..=self.bottom_right.0 {
-                    for j in self.top_left.1..=self.bottom_right.1 {
-                        lights[i as usize][j as usize] += 1;
-                    }
-                }
-            }
-            Kind::Toggle => {
-                for i in self.top_left.0..=self.bottom_right.0 {
-                    for j in self.top_left.1..=self.bottom_right.1 {
-                        lights[i as usize][j as usize] += 2;
-                    }
-                }
-            }
-        }
+    pub fn exec2(&self, lights: &mut Lights) {
+        self.for_each_cell(lights, |cell| match self.kind {
+            Kind::Off => *cell = cell.saturating_sub(1),
+            Kind::On => *cell += 1,
+            Kind::Toggle => *cell += 2,
+        });
     }
 }
 
@@ -99,8 +67,12 @@ fn parse_kind(kind: &str) -> IResult<&str, Kind> {
     ))(kind)
 }
 
-fn parse_coordinate(coord: &str) -> IResult<&str, (u32, u32)> {
-    separated_pair(u32, tag(","), u32)(coord)
+fn parse_coordinate(coord: &str) -> IResult<&str, (usize, usize)> {
+    separated_pair(
+        map(nom_u32, |v| v as usize),
+        tag(","),
+        map(nom_u32, |v| v as usize),
+    )(coord)
 }
 
 fn parse_line(line: &str) -> IResult<&str, Instruction> {
@@ -119,38 +91,29 @@ fn parse_line(line: &str) -> IResult<&str, Instruction> {
     ))
 }
 
-pub fn day6_fst() -> i32 {
-    let mut lights = vec![vec![0; 1000]; 1000];
-    for instruction in input() {
-        instruction.exec(&mut lights);
-    }
-
-    let mut lights_switched_on = 0;
-    for light in lights {
-        for b in light {
-            if b == 1 {
-                lights_switched_on += 1;
-            }
-        }
-    }
-
-    lights_switched_on
+pub fn day6_fst() -> usize {
+    input()
+        .into_iter()
+        .fold(vec![vec![0; 1000]; 1000], |mut lights, instruction| {
+            instruction.exec(&mut lights);
+            lights
+        })
+        .iter()
+        .flatten()
+        .filter(|&b| *b == 1)
+        .count()
 }
 
-pub fn day6_snd() -> i32 {
-    let mut lights = vec![vec![0; 1000]; 1000];
-    for instruction in input() {
-        instruction.exec2(&mut lights);
-    }
-
-    let mut brightnesses = 0_i32;
-    for light in lights {
-        for b in light {
-            brightnesses += b as i32;
-        }
-    }
-
-    brightnesses
+pub fn day6_snd() -> u32 {
+    input()
+        .into_iter()
+        .fold(vec![vec![0; 1000]; 1000], |mut lights, instruction| {
+            instruction.exec2(&mut lights);
+            lights
+        })
+        .iter()
+        .flatten()
+        .sum()
 }
 
 #[cfg(test)]
