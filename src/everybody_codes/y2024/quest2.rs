aef68c497573b9bce4c5f0576ade95fdd72be664
Fabian Bull <spam+github@fbull.de>
2026-05-12 15:30:31 +0200

    Add every body code quest 2 part 2 in Rust

--- src/everybody_codes/y2024/quest2.rs
+++ src/everybody_codes/y2024/quest2.rs
@@ -1,13 +1,13 @@
 use std::fs;
 
 const PART1_FILE: &str = "src/everybody_codes/y2024/quest2_1.txt";
+const PART2_FILE: &str = "src/everybody_codes/y2024/quest2_2.txt";
 
 pub fn quest2_part1() -> usize {
     let content =
         fs::read_to_string(PART1_FILE).expect("Should have been able to read the input file");
 
-    let (header, scripture) = content.split_once("\n\n").unwrap();
-    let runic_words = parse_runic_words(header);
+    let (runic_words, scripture) = parse_runic_words(&content, false);
 
     let mut count = 0;
     for word in &runic_words {
@@ -20,9 +20,37 @@ pub fn quest2_part1() -> usize {
     count
 }
 
-fn parse_runic_words(line: &str) -> Vec<&str> {
-    let index_of_colon = line.find(':').unwrap();
-    line[index_of_colon + 1..].split(',').collect()
+pub fn quest2_part2() -> usize {
+    let content =
+        fs::read_to_string(PART2_FILE).expect("Should have been able to read the input file");
+
+    let (runic_words, scripture) = parse_runic_words(&content, true);
+
+    let mut runes = vec![0; scripture.len()];
+    for word in &runic_words {
+        for i in 0..scripture.len() {
+            if scripture[i..].starts_with(word) {
+                for j in 0..word.len() {
+                    runes[i + j] = 1;
+                }
+            }
+        }
+    }
+    runes.iter().sum()
+}
+
+fn parse_runic_words(notes: &str, reverse: bool) -> (Vec<String>, &str) {
+    let (first, second) = notes.split_once("\n\n").unwrap();
+    let mut words = Vec::new();
+
+    for token in first[6..].split(',') {
+        words.push(String::from(token));
+        if reverse {
+            words.push(token.chars().rev().collect());
+        }
+    }
+
+    (words, second)
 }
 
 #[cfg(test)]
@@ -33,5 +61,10 @@ mod test {
     #[test]
     fn quest2_part1_solution() {
         assert_eq!(quest2_part1(), 29);
+    }
+
+    #[test]
+    fn quest2_part2_solution() {
+        assert_eq!(quest2_part2(), 5236);
     }
 }
